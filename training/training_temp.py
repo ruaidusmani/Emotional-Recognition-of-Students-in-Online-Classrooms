beed8e1ce3458520e95034ae8c880ad980722840
ruaid_usmani <ruaidusmani@gmail.com>
2024-03-24 03:47:26 -0400

    Added CNN4, trained and attained result.

--- training/training_temp.py
+++ training/training_temp.py
@@ -134,3 +134,45 @@ class CNN3(nn.Module):
         # fc layer
         x = self.fc_layer(x)
         return x
+
+# %%
+class CNN4(nn.Module):
+    def __init__(self):
+        super(CNN4, self).__init__()
+        self.name = "CNN4"
+        self.conv_layer = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            #get dimensions of last layer
+            
+        )
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.3),
+            nn.Linear(12 * 12 * 64, 1000),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.3),
+            nn.Linear(1000, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.3),
+            nn.Linear(512, 10)
+        )
+        
+    def forward(self, x, y=None):
+        # conv layers
+        x = self.conv_layer(x)
+        # print(x.shape)
+        # flatten
+        x = x.view(x.size(0), -1)
+        # print(x.shape)
+        # fc layer
+        x = self.fc_layer(x)
+        return x
